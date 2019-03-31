2ce2e125c4b020e12fcaab126730fc20c1076e4d
Quallenauge <Hamsi2k@freenet.de>
2019-03-31 10:21:33 +0200

    bardock: update build fingerprint
    * BQ firmware 2.7.0.
    
    Change-Id: Ib89386be2f5ecf14ab0ca8e8a20d8286550f24e3

--- lineage.mk
+++ lineage.mk
@@ -40,6 +40,6 @@ BOARD_VENDOR := bq
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
         PRODUCT_NAME=bardock \
-        PRIVATE_BUILD_DESC="bardock_bq-user 8.1.0 OPM1.171019.026 1491 release-keys"
+        PRIVATE_BUILD_DESC="bardock_bq-user 8.1.0 OPM1.171019.026 1748 release-keys"
 
-BUILD_FINGERPRINT := bq/bardock/bardock:8.1.0/OPM1.171019.026/1491:user/release-keys
+BUILD_FINGERPRINT := bq/bardock/bardock:8.1.0/OPM1.171019.026/1748:user/release-keys
